0e4daa22d9c8f86a8c375d715a9a6d859a6482ae
Phil Jones <pjones@vqcomms.com>
2021-12-03 21:37:22 +0000

    day 3 part 2

--- day3/main.go
+++ day3/main.go
@@ -68,13 +68,115 @@ func part1(lines []string) (int64, int64, int64, error) {
 
 	fmt.Printf("Gamma = %d\n", gammaInt)
 	fmt.Printf("Epsilon = %d\n", epsilonInt)
-	fmt.Printf("Power = %d\n", gammaInt * epsilonInt)
+	fmt.Printf("Power = %d\n", gammaInt*epsilonInt)
 
 	return gammaInt, epsilonInt, gammaInt * epsilonInt, nil
+}
+
+func part2(lines []string) (int64, int64, int64, error) {
+	numberOfBitsPerLine := len(lines[0])
+
+	o2Lines := lines
+	co2Lines := lines
+
+	for i := 0; i < numberOfBitsPerLine; i++ {
+		// recalculate each iteration as we're changing the array length
+		numberOfLines := len(o2Lines)
+
+		if numberOfLines == 1 {
+			continue
+		}
+
+		on := 0
+		off := 0
+		for ii := 0; ii < numberOfLines; ii++ {
+			bit, _ := strconv.Atoi(string(string(o2Lines[ii])[i]))
+
+			if bit == 1 {
+				on++
+			} else {
+				off++
+			}
+		}
+
+		keep := 0
+
+		if on >= off {
+			keep = 1
+		}
+
+		var newO2Lines []string
+
+		for ii := 0; ii < numberOfLines; ii++ {
+			bit, _ := strconv.Atoi(string(string(o2Lines[ii])[i]))
+
+			if bit == keep {
+				newO2Lines = append(newO2Lines, o2Lines[ii])
+			}
+		}
+
+		o2Lines = newO2Lines
+	}
+
+	for i := 0; i < numberOfBitsPerLine; i++ {
+		// recalculate each iteration as we're changing the array length
+		numberOfLines := len(co2Lines)
+
+		if numberOfLines == 1 {
+			continue
+		}
+
+		on := 0
+		off := 0
+		for ii := 0; ii < numberOfLines; ii++ {
+			bit, _ := strconv.Atoi(string(string(co2Lines[ii])[i]))
+
+			if bit == 1 {
+				on++
+			} else {
+				off++
+			}
+		}
+
+		keep := 1
+
+		if on >= off {
+			keep = 0
+		}
+
+		var newCO2Lines []string
+
+		for ii := 0; ii < numberOfLines; ii++ {
+			bit, _ := strconv.Atoi(string(string(co2Lines[ii])[i]))
+
+			if bit == keep {
+				newCO2Lines = append(newCO2Lines, co2Lines[ii])
+			}
+		}
+
+		co2Lines = newCO2Lines
+	}
+
+	if len(o2Lines) > 1|len(co2Lines) {
+		return 0, 0, 0, os.ErrInvalid
+	}
+
+	fmt.Printf("02 = %s\n", o2Lines[0])
+	o2, _ := strconv.ParseInt(o2Lines[0], 2, 64)
+
+	fmt.Printf("CO2 = %s\n", co2Lines[0])
+	co2, _ := strconv.ParseInt(co2Lines[0], 2, 64)
+
+	fmt.Printf("O2 = %d\n", o2)
+	fmt.Printf("CO2 = %d\n", co2)
+	fmt.Printf("Power = %d\n", o2*co2)
+
+	return o2, co2, o2 * co2, nil
 }
 
 func main() {
 	lines, _ := readInput("./day3/input")
 
-	part1(lines)
+	// part1(lines)
+	part2(lines)
 }
--- day3/main_test.go
+++ day3/main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"testing"
 )
 
@@ -20,17 +21,52 @@ func TestPart1(t *testing.T) {
 		"01010",
 	}
 
-	 gamma, epsilon, power, _ := part1(lines)
+	gamma, epsilon, power, _ := part1(lines)
 
-	 if gamma != 22 {
-		 t.Fatalf("Expected gamma to be 22 but got %d", gamma)
-	 }
+	if gamma != 22 {
+		t.Fatalf("Expected gamma to be 22 but got %d", gamma)
+	}
 
-	 if epsilon != 9 {
-		 t.Fatalf("Expected epsilon to be 9 but got %d", epsilon)
-	 }
+	if epsilon != 9 {
+		t.Fatalf("Expected epsilon to be 9 but got %d", epsilon)
+	}
 
-	 if power != 198 {
-		 t.Fatalf("Expected power to be 198 but got %d", power)
-	 }
+	if power != 198 {
+		t.Fatalf("Expected power to be 198 but got %d", power)
+	}
+}
+
+func TestPart2(t *testing.T) {
+	lines := []string{
+		"00100",
+		"11110",
+		"10110",
+		"10111",
+		"10101",
+		"01111",
+		"00111",
+		"11100",
+		"10000",
+		"11001",
+		"00010",
+		"01010",
+	}
+
+	oxygen, co2, life, _ := part2(lines)
+
+	fmt.Printf("Oxygen = %d\n", oxygen)
+	fmt.Printf("CO2 = %d\n", co2)
+	fmt.Printf("Life = %d\n", life)
+
+	if oxygen != 23 {
+		t.Fatalf("Expected oxygen level to be 23 but got %d", oxygen)
+	}
+
+	if co2 != 10 {
+		t.Fatalf("Expected co2 level to be 10 but got %d", co2)
+	}
+
+	if life != 230 {
+		t.Fatalf("Expected life support to be 230 but got %d", life)
+	}
 }
